bddd8f3765073779126771ae6abf4ab052973b5f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-05-04 12:26:37 +0800

    Update BootupState.hx

--- source/BootupState.hx
+++ source/BootupState.hx
@@ -73,8 +73,8 @@ class BootupState extends MusicBeatState
 		#end*/
 		
 		#if sys
-		if (!sys.FileSystem.exists("/assets/replays"))
-			sys.FileSystem.createDirectory("/assets/replays");
+		if (!sys.FileSystem.exists("assets/replays"))
+			sys.FileSystem.createDirectory("assets/replays");
 		#end
 
 		@:privateAccess
@@ -104,22 +104,7 @@ class BootupState extends MusicBeatState
 
 		FlxG.sound.playMusic(Paths.music('menuIntro'));
 		FlxG.sound.music.stop();
-		var initSonglist = CoolUtil.coolTextFile(Paths.txt('freeplaySonglist'));
-		for (i in 0...initSonglist.length)
-		{
-			var data:Array<String> = initSonglist[i].split(':');
-			var songHighscore = StringTools.replace(data[0], " ", "-");
-				switch (songHighscore) {
-					case 'Dad-Battle': songHighscore = 'Dadbattle';
-					case 'Philly-Nice': songHighscore = 'Philly';
-			}
-			if (!loadedStuff) {
-				FlxG.sound.cache(Paths.inst(songHighscore));
-				if (FileSystem.exists(Paths.instEXcheck(songHighscore))) {
-					FlxG.sound.cache(Paths.instEX(songHighscore));
-				}
-			}
-		}
+	
 		
 		loadedStuff = true;
 
